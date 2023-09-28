254a74297d09e6cafd8bae059d872967d8cf3788
befloresfarias <bernardo.2021319283@aluno.iffar.edu.br>
2023-09-28 14:21:54 -0300

    Update crudPorta.php
    crud php porta

--- crudPorta.php
+++ crudPorta.php
@@ -5,15 +5,13 @@ include("conecta.php");
 $cor = $_POST["cor"];
 $modelo = $_POST["modelo"];
 $valor = $_POST["valor"];
-$id_Item = "";
 
-$sql = "INSERT INTO funcionario (cor, modelo, valor, id_Item) VALUES ('$cor', $modelo, '$valor', $id_Item)"; // pede para inserir em tal tabela no banco
+$sql = "INSERT INTO porta (cor, modelo, valor) VALUES ('$cor', '$modelo', $valor)"; // pede para inserir em tal tabela no banco
 $resultado = mysqli_query($conexao, $sql);
 
-if ($resultado == true) {
-    header("Location: consultarReg.php?result=true");
-} else {
-    header("Location: consultarReg.php?result=false");
-}
-
+// if ($resultado == true) {
+//     header("Location: consultar.php?result=true");
+// } else {
+//     header("Location: consultar.php?result=false");
+// }
 ?>
